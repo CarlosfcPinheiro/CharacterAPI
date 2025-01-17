1339f9354915890a905d6c081f3520c471841b8b
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-01-17 18:24:20 -0300

    refactor: Adjusting entities attributes

--- db/createdb_script/script.sql
+++ db/createdb_script/script.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA charapi;
+CREATE SCHEMA IF NOT EXISTS charapi;
 SET search_path TO charapi;
 
 CREATE TABLE IF NOT EXISTS "user"(
@@ -6,10 +6,12 @@ CREATE TABLE IF NOT EXISTS "user"(
     username VARCHAR(20) NOT NULL,
     email VARCHAR(30) NOT NULL,
     password VARCHAR(20),
+    char_count INT NOT NULL,
 
     created_at timestamp DEFAULT current_timestamp
 );
 
+CREATE TYPE FACE AS ENUM('Sad', 'Happy', 'Angry', 'Neutral');
 CREATE TYPE HEAD AS ENUM('Triangle', 'Square', 'Circle');
 CREATE TYPE ACCESSORY AS ENUM('Glasses', 'Moustache', 'Hat');
 CREATE TYPE ITEM AS ENUM('Cane', 'Staff', 'Paper');
@@ -18,9 +20,10 @@ CREATE TABLE IF NOT EXISTS char(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     userid UUID NOT NULL,
     charname VARCHAR(20) NOT NULL,
-    head HEAD NOT NULL,
-    accessory ACCESSORY NOT NULL,
-    item ITEM NOT NULL,
+    face_type FACE NOT NULL,
+    head_type HEAD NOT NULL,
+    accessory_type ACCESSORY NOT NULL,
+    item_type ITEM NOT NULL,
     created_at timestamp DEFAULT current_timestamp,
 
     CONSTRAINT fk_userid FOREIGN KEY (userid) REFERENCES "user" (id)
