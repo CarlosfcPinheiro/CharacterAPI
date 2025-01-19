e886a23c74ed4acac593cdee6625f5e11bc8a2cc
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-01-19 17:03:59 -0300

    feat: Adding std User and Char controllers

---
+++ controllers/char.js
@@ -0,0 +1,45 @@
+// Importing usefull packages
+const express = require('express');
+// Importing models
+const Char = require('../models/char.js');
+const User = require('../models/user.js');
+
+const getAllChars = async(req, res) => {
+    res.send('getAllChars');
+    res.end();
+}
+
+const getCharsByUserId = async(req, res) => {
+    res.send('getCharsByUserId');
+    res.end();
+}
+
+const getSingleCharById = async(req, res) => {
+    res.send('getSingleCharById');
+    res.send();
+}
+
+const createChar = async(req, res) => {
+    res.send('createChar');
+    res.end();
+}
+
+const changeCharById = async(req, res) => {
+    res.send('changeCharById');
+    res.end();
+}
+
+const deleteCharById = async(req, res) => {
+    res.send('delteCharById');
+    res.end();
+}
+
+// Exporting methods
+module.exports = {
+    getAllChars,
+    getCharsByUserId,
+    getSingleCharById,
+    createChar,
+    changeCharById,
+    deleteCharById
+}
