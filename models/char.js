676ffed69a4a53cfbab9d4c311574e587db3aa24
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-01-19 17:07:47 -0300

    feat: Adding Char model

---
+++ models/char.js
@@ -0,0 +1,60 @@
+// Importing usefull packages
+const {Sequelize, DataTypes} = require('sequelize');
+// Getting sequelize instance
+const {sequelize}= require('../db/db.js');
+
+// Creating ENUMs
+const FACE_ENUM = ['Sad', 'Happy', 'Angry', 'Neutral'];
+const HEAD_ENUM = ['Triangle', 'Square', 'Circle'];
+const ACCESSORY_ENUM = ['Glasses', 'Moustache', 'Hat'];
+const ITEM_ENUM = ['Cane', 'Staff', 'Paper'];
+
+// Creating char entity
+const Char = sequelize.define('char', 
+    {
+        id:{
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        userid:{
+            type: DataTypes.UUID,
+            allowNull: false,
+        },
+        face_type:{
+            type: DataTypes.ENUM,
+            values: FACE_ENUM,
+            allowNull: false,
+        },
+        head_type:{
+            type: DataTypes.ENUM,
+            values: HEAD_ENUM,
+            allowNull: false,
+        },
+        accessory_type:{
+             type: DataTypes.ENUM,
+             values: ACCESSORY_ENUM,
+             allowNull: false,
+        },
+        item_type:{
+            type: DataTypes.ENUM,
+            values: ITEM_ENUM,
+            allowNull: false,
+        },
+        created_at:{
+            type: DataTypes.DATE,
+            defaultValue: Sequelize.NOW,
+        }
+    },
+    {
+       schema: 'charapi',
+        tableName: 'char',
+        timestamps: false,
+    }
+);
+
+// User table Association
+Char.belongsTo(require('./user.js'));
+
+// Exporting char model
+module.exports = Char;
