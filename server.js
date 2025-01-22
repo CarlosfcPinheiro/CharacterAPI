6c0d0154cc694aca3a91e8bcf6ae9fd472ed26ac
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-01-21 23:23:47 -0300

    feat: Adding cors middleware

--- server.js
+++ server.js
@@ -3,14 +3,26 @@ const morgan = require('morgan');
 const express = require('express');
 require('dotenv').config();
 
+// Import cors config
+const corsConfig = require('./utils/cors.js');
+
+// Import Router
+const userRouter = require('./routes/user.js');
+const charRouter = require('./routes/char.js');
+
 // Database test connection
 const connectionTestSyncDB = require('./db/connectionTestSyncDB.js');
 
 // Config server
 const server = express();
 const port = 3000 || process.env.PORT;
+const std_endpoint = '/api/v1'
 
 // Applying middlewares
+server.options('*', corsConfig);
+server.use(express.json());
+server.use(`${std_endpoint}/user`, userRouter);
+server.use(`${std_endpoint}/char`, charRouter);
 // server.use(morgan('tiny'));
 server.use(morgan('dev'));
 
