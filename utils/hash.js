de11b1f120796357d2c1f6d9cad35729155e016d
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-01-29 23:24:57 -0300

    feat: Adding hash and verify function

---
+++ utils/hash.js
@@ -0,0 +1,19 @@
+// Get bycriptjs package
+const bcrypt = require('bcryptjs');
+
+// Generate hash
+const hashPassword = async(password) => {
+    const saltR = 5;
+    const hashedPassword = await bcrypt.hash(password, saltR);
+    
+    return hashedPassword;
+}
+
+// Verify password
+const verifyPassword = async(password, hashedPassword) => {
+    const verify = await bcrypt.compare(password, verifyPassword);
+    return verify;
+}
+
+// Exporting hash utils
+module.exports = {hashPassword, verifyPassword};
