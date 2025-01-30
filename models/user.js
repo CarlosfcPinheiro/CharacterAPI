be94c4ebfc31cd54482614fa2cd4718483a09295
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-01-29 23:26:07 -0300

    refactor: Change char quantity on password

--- models/user.js
+++ models/user.js
@@ -20,7 +20,7 @@ const User = sequelize.define('user',
             allowNull: false,
         },
         password:{
-            type: DataTypes.STRING(20),
+            type: DataTypes.STRING(60),
             allowNull: false,
         },
         char_count:{
