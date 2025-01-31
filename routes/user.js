065bd71aba330e8494090f2214d0e89394c6edf7
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-01-30 23:46:49 -0300

    refactor: Adding id param on delete route

--- routes/user.js
+++ routes/user.js
@@ -23,7 +23,7 @@ router.get('/logout', logoutUser);
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
-router.delete('/', deleteUser);
+router.delete('/id/:id', deleteUser);
 
 router.patch('/id/:id', changeCredentialsUser);
 
