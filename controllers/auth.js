54e6ecf0395a2fccddc854a9373a3462223d23c9
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-02-01 17:21:32 -0300

    feat: Auth controller and route

---
+++ controllers/auth.js
@@ -0,0 +1,55 @@
+// Importing User model
+const User = require('../models/user.js');
+// Importing jwt
+const jwt = require('jsonwebtoken');
+// Importing hash functions
+const {verifyPassword} = require('../utils/hash.js');
+
+const loginUser = async(req, res) => {
+    const {username, password} = req.body;
+    try{
+        const user = await User.findOne({
+            where:{
+                username: username
+            },
+        });
+        if (!user){
+            return res.status(404).json({
+                message: `User with name ${username} not found.`,
+            });
+        }
+
+        const checkPassword = await verifyPassword(password, user.password);
+        if (!checkPassword){
+            return res.status(401).json({
+                message: 'Invalid or wrong password.'
+            });
+        }
+
+        const token = jwt.sign({id: user.id}, process.env.SECRET_KEY, {expiresIn: '1h'});
+        res.status(200).json({
+            message: 'Login successfully.',
+            id: user.id,
+            authToken: token,
+        });
+    } catch(err){
+        console.log(err);
+        res.status(500).json({
+            message: 'User was not logged in.',
+            error: err.name,
+        });
+    }
+}
+
+const verifyTokenUser = async(req, res) => {
+    res.send('verifyTokenUser');
+    res.end();
+}
+
+const logoutUser = async(req, res) => {
+    res.send('logoutUser');
+    res.end();
+}
+
+// Exporting methods
+module.exports = {loginUser, verifyTokenUser, logoutUser}
