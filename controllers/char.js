d2dc4b2e0fc87e79d6efc9ad6d23bcc82e6f8bef
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-02-14 15:51:52 -0300

    feat: Adding GET all chars, GET char by userid and POST new char

--- controllers/char.js
+++ controllers/char.js
@@ -1,15 +1,51 @@
 // Importing models
+const { Op } = require('sequelize');
 const Char = require('../models/char.js');
 const User = require('../models/user.js');
 
 const getAllChars = async(req, res) => {
-    res.send('getAllChars');
-    res.end();
+    const {sortBy='charname', order='ASC', username} = req.query;
+    try{
+        const chars = await Char.findAll({
+            order: [[sortBy, order.toUpperCase()]],
+            // include option/attribute can fetch multipple associated models
+            include:[
+                {
+                    model: User,
+                    as: 'user-creator',
+                    attributes: ['username']
+                }
+            ]
+        });
+        res.status(200).json(chars);
+    } catch(err){
+        console.log(err);
+        res.status(500).json({
+            message: 'Error to get characters.',
+        });
+    }
 }
 
 const getCharsByUserId = async(req, res) => {
-    res.send('getCharsByUserId');
-    res.end();
+    const {userId} = req.params;
+    console.log(req.params);
+    const {sortBy='charname', order='ASC'} = req.query;
+    try{
+        const chars = await Char.findAll({
+            where: { userid:userId },
+            order: [[sortBy, order.toUpperCase()]],
+        });
+        res.status(200).json({
+            chars,
+            entities_count: Object.keys(chars).length,
+        });
+    } catch(err){
+        console.log(err);
+        res.status(500).json({
+            message: 'Error to get characters by userid.',
+            error: err.name,
+        });
+    }
 }
 
 const getSingleCharById = async(req, res) => {
@@ -18,8 +54,40 @@ const getSingleCharById = async(req, res) => {
 }
 
 const createChar = async(req, res) => {
-    res.send('createChar');
-    res.end();
+    const {charname, face_type, head_type, accessory_type, item_type} = req.body;
+    try{
+        const char = await Char.findOne({
+            where: {
+                charname: charname,
+            }
+        });
+        // Checks if the charname provided already in use
+        if (char){
+            return res.status(409).json({
+                message: 'The char with the provided charname already exists."='
+            });
+        }
+        const userid = req.user.id;
+        const newChar = await Char.create({
+            charname: charname,
+            userid: userid,
+            face_type: face_type,
+            head_type: head_type,
+            accessory_type: accessory_type,
+            item_type: item_type,
+        });
+
+        res.status(201).json({
+            message: 'Char created successfully.',
+            newChar: newChar
+        });
+    } catch(err){
+        console.log(err);
+        res.status(500).json({
+            message: 'Character was not created.',
+            error: err.name,
+        });
+    }
 }
 
 const changeCharById = async(req, res) => {
