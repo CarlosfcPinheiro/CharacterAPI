92bcc9f6f5c32fe3e3dd3fbaca75ac5a522163dc
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-02-19 23:38:42 -0300

    feat: Adding new test to /user route

--- tests/userRoutes.test.js
+++ tests/userRoutes.test.js
@@ -1,17 +1,30 @@
 // Importing packages
 const sum = require('../utils/sum.js');
-// const request = require('supertest');
-// const server = require('../server.js');
-
-// describe('Testing API User model', () => {
-//     test('GET /user may return users list with status 200 OK', async() => {
-//         const response = await request(server).get('/user');
-
-//         expect(response.statusCode).toBe(200);
-//         expect(response.body).toBeInstanceOf(Array);
-//     });
-// });
+// tests packages
+const {describe, it, expect, beforeAll, afterAll} = require('@jest/globals');
+const request = require('supertest');
+const server = require('../server.js');
+// DB resources
+const connectionTestSyncDB = require('../db/connectionTestSyncDB.js');
+const { sequelize } = require('../db/db.js');
 
 test('Sum 2 + 3 may result in 5', () => {
     expect(sum(2, 3)).toBe(5);
+});
+
+beforeAll(async () => {
+    await connectionTestSyncDB();
+});
+
+afterAll(async () => {
+    await sequelize.close();
+});
+
+describe('Testing /user requests', () => {
+    it('May response users array with correct attributes', async () => {
+        const res = await request(server).get('/api/v1/user');
+        expect(res.statusCode).toBe(200);
+        expect(Array.isArray(res.body.users)).toBe(true);
+        expect(res.body.users.length).toBeGreaterThan(0);
+    });
 });
